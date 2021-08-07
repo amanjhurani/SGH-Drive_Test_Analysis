fff40cc17a2658495c25bde682390cb699dff0b7
amanjhurani <amanjhurani5@gmail.com>
2021-08-07 13:43:53 +0530

    file upload and get names apis

--- backend/bsnl_backend/base/urls.py
+++ backend/bsnl_backend/base/urls.py
@@ -6,5 +6,6 @@ from . import views
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/v1/get_data', views.get_data, name="get_data")
+    path('api/v1/upload_file', views.upload_file, name="upload_file"),
+    path('api/v1/all_files', views.all_files, name="all_files")
 ]
--- backend/bsnl_backend/base/views.py
+++ backend/bsnl_backend/base/views.py
@@ -8,6 +8,31 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import permissions
 
-@api_view(['GET', 'DELETE', 'PUT'])
-def get_data(request):
-   return Response(status=200, data={"Hi it's working! Please"})
+import csv
+import os
+
+BASE_PATH = "./files"
+
+@api_view(['GET', 'DELETE', 'PUT', 'POST'])
+def upload_file(request):
+    uploaded_filename = request.FILES['file'].name
+    try:
+        os.mkdir(os.path.join(BASE_PATH))
+    except:
+        pass
+    full_filename = os.path.join(BASE_PATH, uploaded_filename)
+    fout = open(full_filename, 'wb+')
+    file_data = request.FILES['file']
+    for chunk in file_data.chunks():
+        fout.write(chunk)
+    fout.close()
+    return Response(status=200, data={"sucess"})
+
+@api_view(['GET'])
+def all_files(request):
+    try:
+        os.mkdir(os.path.join(BASE_PATH))
+    except:
+        pass
+    file_list = os.listdir(BASE_PATH)
+    return Response(status=200, data=file_list)
