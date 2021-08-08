f30ed28106431b6a8fc998e77f0d8b93fb335072
amanjhurani <amanjhurani5@gmail.com>
2021-08-08 14:08:38 +0530

    all graph apis

--- backend/bsnl_backend/base/urls.py
+++ backend/bsnl_backend/base/urls.py
@@ -7,5 +7,8 @@ from . import views
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('api/v1/upload_file', views.upload_file, name="upload_file"),
-    path('api/v1/all_files', views.all_files, name="all_files")
+    path('api/v1/all_files', views.all_files, name="all_files"),
+    path('api/v1/display_rxtx', views.display_rxtx, name="display_rxtx"),
+    path('api/v1/display_rscp', views.display_rscp, name="display_rscp"),
+    path('api/v1/display_eclo', views.display_eclo, name="display_eclo")
 ]
--- backend/bsnl_backend/base/views.py
+++ backend/bsnl_backend/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from numpy.core.numeric import NaN
 
 # Create your views here.
 from rest_framework import viewsets
@@ -10,6 +11,8 @@ from rest_framework import permissions
 
 import csv
 import os
+import pandas as pd
+import math as math
 
 BASE_PATH = "./files"
 
@@ -36,3 +39,61 @@ def all_files(request):
         pass
     file_list = os.listdir(BASE_PATH)
     return Response(status=200, data=file_list)
+
+@api_view(['GET', 'POST'])
+def display_rscp(request):
+
+    file_name = request.data.get('file_name')
+    data_file = os.path.join(BASE_PATH, file_name)
+    rscp_df = pd.read_csv(data_file)
+    rscp_json = []
+
+    for time, lat, long, rscp in zip(rscp_df['Time'].to_list(), rscp_df['Latitude'].to_list(), rscp_df['Longitude'].to_list(), rscp_df['Active RSCP (dBm)(0)'].to_list(), ):
+        tmp = dict()
+        tmp["time_stamp"] = time
+        tmp["coordinate"] = [lat, long]
+        tmp["active_rscp"] = rscp
+
+        rscp_json.append(tmp)
+
+    return Response(status=200, data=rscp_json)
+
+@api_view(['GET', 'POST'])
+def display_rxtx(request):
+
+    file_name = request.data.get('file_name')
+    data_file = os.path.join(BASE_PATH, file_name)
+    rxtx_df = pd.read_csv(data_file)
+    rxtx_json = []
+
+    for time, lat, long, rx_power, tx_power in zip(rxtx_df['Time'].to_list(), rxtx_df['Latitude'].to_list(), rxtx_df['Longitude'].to_list(), rxtx_df['Rx Power (dBm)'].to_list(), rxtx_df['Tx Power (dBm)'].to_list()):
+        if  math.isnan(tx_power):
+            tx_power = None
+            
+        tmp = dict()
+        tmp["time_stamp"] = time
+        tmp["coordinate"] = [lat, long]
+        tmp["rxtx_power"] = [rx_power, tx_power]
+
+        rxtx_json.append(tmp)
+
+    return Response(status=200, data=rxtx_json)
+
+
+@api_view(['GET', 'POST'])
+def display_eclo(request):
+
+    file_name = request.data.get('file_name')
+    data_file = os.path.join(BASE_PATH, file_name)
+    eclo_df = pd.read_csv(data_file)
+    eclo_json = []
+
+    for time, lat, long, eclo in zip(eclo_df['Time'].to_list(), eclo_df['Latitude'].to_list(), eclo_df['Longitude'].to_list(), eclo_df['Total Agg EcIo (dB)'].to_list(), ):
+        tmp = dict()
+        tmp["time_stamp"] = time
+        tmp["coordinate"] = [lat, long]
+        tmp["active_eclo"] = eclo
+
+        eclo_json.append(tmp)
+
+    return Response(status=200, data=eclo_json)
